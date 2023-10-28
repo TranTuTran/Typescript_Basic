5c0732f3dd8c5a3c098b71ec3b617c69266b292b
adeleyeayodeji <biodunhi@gmail.com>
2023-10-28 19:06:19 +0700

    Why TypeScript

---
+++ projects/lesson4/lesson4.js
@@ -0,0 +1,15 @@
+//perfect
+const sum = (x, y) => {
+    return x + y;
+}
+
+console.log(">>> check sum1 = ", sum(5, 10));
+
+//need to validate 
+
+const sum2 = (x, y) => {
+    return x + y;
+}
+
+console.log(">>> check sum2 =", sum2('name', 10));
+
