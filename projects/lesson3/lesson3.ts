6db6d6da56b4148979255866aff699a1c37739e1
adeleyeayodeji <biodunhi@gmail.com>
2023-10-28 17:11:40 +0700

    Hello World vơi TypeScript

---
+++ projects/lesson3/lesson3.ts
@@ -0,0 +1,8 @@
+let message: string = "Hello, World!";
+console.log(message)
+
+
+/* để phiên dịch qua javascript có 2 cách:
+1. ts-node .\projects\lesson3\lesson3.ts
+2. tsc .\projects\lesson3\lesson3.ts  
+*/ 
