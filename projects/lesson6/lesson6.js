79718ed60072a7a646981d56a63fa03300e3cae5
adeleyeayodeji <biodunhi@gmail.com>
2023-10-29 14:59:00 +0700

    Type Annotations-Keyword Type

---
+++ projects/lesson6/lesson6.js
@@ -0,0 +1,12 @@
+// Type Annotations : loại chú thích 
+// let count = 1; //ok
+
+// count = 'name';
+
+// console.log(">> check name: ", count)
+
+let names = ['Eric', 'Hỏi dân IT', 'Typescript'] 
+
+names.push(25)
+
+console.log(">> check name: ", names)
---
+++ projects/lesson6/lesson6.ts
@@ -0,0 +1,10 @@
+// Type Annotations : loại chú thích 
+// let count1: number; //ok
+
+// count1= 10;
+
+// console.log(">> check name: ", count1)
+
+let names2: string[] = ['Eric', 'Hỏi dân IT', 'Typescript'] // array chỉ bao gồm string
+names2.push('25')
+console.log(">> check name: ", names2)
