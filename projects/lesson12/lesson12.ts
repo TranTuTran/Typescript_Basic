67e99cb49ead09d828bab23c38dffc04f7e58ff4
adeleyeayodeji <biodunhi@gmail.com>
2023-10-29 19:42:09 +0700

    Array Type

---
+++ projects/lesson12/lesson12.ts
@@ -0,0 +1,8 @@
+let test: (string | number)[] = ['Eric'];
+
+test.push('hoi dan it');
+test.push(25);
+console.log(">>> check array: ", test)
+
+// cách khai báo khi muốn mảng có chứa nhiều dữ liệu 
+// hoặc khai báo giá trị trước  let test = ['Eric', 15]; => tự định nghĩa kiểu dữ liệu 
